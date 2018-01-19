e27bbe49b28c24af34231492effd4418fa1b5751
Pierre Grimaud <pierre.grimaud@bigyouth.fr>
2018-01-19 18:07:38 +0100

    remove some issues

--- Listener/PageCacheListener.php
+++ Listener/PageCacheListener.php
@@ -11,6 +11,7 @@ namespace Bigyouth\BigyouthPageCacheBundle\Listener;
 use Bigyouth\BigyouthPageCacheBundle\Services\PageCacheService;
 use Symfony\Component\Cache\Adapter\TagAwareAdapter;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -47,25 +48,37 @@ class PageCacheListener implements EventSubscriberInterface
      */
     public function onKernelRequest(GetResponseEvent $event)
     {
+        /** @var Request $request */
         $request = $event->getRequest();
 
-        if ($this->cacheService->isEnabled() && $event->getRequestType() == 1) {
-            if (!$this->cacheService->isExclude($request) && (!$this->tokenStorage->getToken()
-                    || $this->tokenStorage->getToken()->getUser() == 'anon.')) {
-                /** @var TagAwareAdapter $cache */
-                $cache = $this->cacheService->getAdapter();
+        if ($this->isCacheContext($event, $request)) {
+            /** @var TagAwareAdapter $cache */
+            $cache = $this->cacheService->getAdapter();
 
-                if ($request->getMethod() == 'GET') {
-                    $cacheKey = $this->cacheService->getCacheKey($request);
+            $cacheKey = $this->cacheService->getCacheKey($request);
 
-                    $data = $cache->getItem($cacheKey);
+            $data = $cache->getItem($cacheKey);
 
-                    if ($data->isHit()) {
-                        $event->setResponse(new Response($data->get()));
-                    }
-                }
+            if ($data->isHit()) {
+                $event->setResponse(new Response($data->get()));
             }
         }
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     * @param Request $request
+     * @return bool
+     */
+    private function isCacheContext(GetResponseEvent $event, Request $request)
+    {
+        return $this->cacheService->isEnabled()
+            && $event->getRequestType() == 1
+            && !$this->cacheService->isExclude($request)
+            && (!$this->tokenStorage->getToken()
+                || $this->tokenStorage->getToken()->getUser() == 'anon.'
+            )
+            && $request->getMethod() == 'GET';
     }
 
     /**
